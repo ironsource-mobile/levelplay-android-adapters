94863d6837003530526e8292641aaf77fe412295
jenkins-build-machine <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>
2022-05-23 12:32:30 +0300

    Google (AdMob and Ad Manager) Adapter 4.3.28 Support SDK 20.6.0

--- Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/build/generated/source/buildConfig/release/com/ironsource/adapters/admob/BuildConfig.java
+++ Adapters/Google (AdMob and Ad Manager)/ISAdMobAdapter/build/generated/source/buildConfig/release/com/ironsource/adapters/admob/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.ironsource.adapters.admob";
   public static final String BUILD_TYPE = "release";
   // Field from default config.
-  public static final String GitHash = "86aa8a1";
+  public static final String GitHash = "89a50c1";
   // Field from default config.
   public static final String VERSION_NAME = "4.3.28";
 }
